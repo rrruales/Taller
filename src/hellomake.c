6fcce5293cea24e4b2a17e4934ccc5114064ac0e
Richard <rrruales@espol.edu.ec>
2017-11-08 00:15:02 -0500

    Solicitar palabra por pantalla

--- src/hellomake.c
+++ src/hellomake.c
@@ -1,7 +1,22 @@
 #include <hellomake.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX 100
+int esPalindromo(char *palabra);
 
 int main(){
 	// llamar a una función en otro archivo
 	myPrintHelloMake ();
+
+	char line[MAX];
+	int a = 0;
+	printf("Escriba palabra para comprobar si es palíndromo: \n");
+	scanf("%s",line);
+	a = esPalindromo(line);
+	if(a == 0)
+		printf("No es palindromo\n");
+	else
+		printf("Es palindromo\n");
 	return(0);
 }
